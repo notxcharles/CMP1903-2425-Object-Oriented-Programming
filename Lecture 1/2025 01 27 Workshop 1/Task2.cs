561cf15e8caabaa44e64177a531e3eaba5d03ffa
notxcharles <35784256+notxcharles@users.noreply.github.com>
2025-01-27 16:27:47 +0000

    Task2

---
+++ Lecture 1/2025 01 27 Workshop 1/Task2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2025_01_27_Workshop_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] random_integers = GenerateRandomIntegers();
+            
+            foreach (int integer in random_integers)
+            {
+                Console.WriteLine(integer);
+            }
+            return;
+        }
+        static int[] GenerateRandomIntegers()
+        {
+            Random random = new Random();
+            int[] random_integers = new int[100];
+            for (int i = 0; i < 100; i++)
+            {
+                random_integers[i] = random.Next();
+            }
+            return random_integers;
+        }
+    }
+}
+
