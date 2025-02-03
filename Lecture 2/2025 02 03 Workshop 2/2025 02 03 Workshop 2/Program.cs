96d9c3e9e4882a1807faf82fc052de5c47abc6ef
notxcharles <35784256+notxcharles@users.noreply.github.com>
2025-02-03 16:02:40 +0000

    Workshop 2 sln

---
+++ Lecture 2/2025 02 03 Workshop 2/2025 02 03 Workshop 2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2025_02_03_Workshop_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
