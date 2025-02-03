69bbc3dbf79a6f2e311df7395d1fc6b4d2cf5a7e
notxcharles <35784256+notxcharles@users.noreply.github.com>
2025-02-03 16:22:14 +0000

    Task 1

--- Lecture 2/2025 02 03 Workshop 2/2025 02 03 Workshop 2/Program.cs
+++ Lecture 2/2025 02 03 Workshop 2/2025 02 03 Workshop 2/Program.cs
@@ -8,8 +8,81 @@ namespace _2025_02_03_Workshop_2
 {
     internal class Program
     {
+        class Student
+        {
+            private string m_studentName;
+            private string m_moduleName;
+            private int m_grade;
+            public Student(string studentName, int grade, string moduleName)
+            {
+                this.m_studentName = studentName;
+                this.m_moduleName = moduleName;
+                this.m_grade = grade;
+            }
+
+            public string StudentName
+            {
+                get { return m_studentName; }
+                set
+                {
+                    if (string.IsNullOrEmpty(value))
+                    {
+                        Console.WriteLine("Cannot be empty");
+                        m_studentName = "NoName";
+                    }
+                    else
+                    {
+                        m_studentName = value;
+                    }
+                }
+            }
+
+            public int Grade
+            {
+                get { return m_grade; }
+                set
+                {
+                    if (value >= 0 & value <= 100)
+                    {
+                        m_grade = value;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Grade must be above or equal 0 and less than  or equal to 100");
+                        m_grade = 0;
+                    }
+                }
+            }
+
+            public string ModuleName
+            {
+                get { return m_moduleName; }
+                set
+                {
+                    if (string.IsNullOrEmpty(value))
+                    {
+                        Console.WriteLine("Cannot be empty");
+
+                    }
+                    else
+                    {
+                        m_moduleName = value;
+                    }
+                }
+            }
+
+            public void DisplayInfo()
+            {
+                Console.WriteLine($"The student: {m_studentName} attended the module {m_moduleName} and was marked with {m_grade}.");
+            }
+        }
         static void Main(string[] args)
         {
+            Student student = new Student("Alice", 45, "Object-Oriented Programming");
+            student.Grade = 50;
+            student.ModuleName = "oop";
+            student.StudentName = "charles";
+            student.DisplayInfo();
         }
     }
 }
