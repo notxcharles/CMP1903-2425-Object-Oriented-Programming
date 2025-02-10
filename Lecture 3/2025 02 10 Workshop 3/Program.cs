aae92e1d319fb794f9e6e575b512239e58b45a73
Charles <35784256+notxcharles@users.noreply.github.com>
2025-02-10 13:19:37 +0000

    Task2
    I can read again, I think

--- Lecture 3/2025 02 10 Workshop 3/Program.cs
+++ Lecture 3/2025 02 10 Workshop 3/Program.cs
@@ -8,6 +8,18 @@ namespace _2025_02_10_Workshop_3
 {
     internal class Program
     {
+        class Dice
+        {
+            static Random random;
+            public Dice()
+            {
+                random = new Random();
+            }
+            public int Roll(int rangeStart = 1, int rangeEnd = 6)
+            {
+                return random.Next(rangeStart, rangeEnd);
+            }
+        }
         class Game
         {
             public Game()
