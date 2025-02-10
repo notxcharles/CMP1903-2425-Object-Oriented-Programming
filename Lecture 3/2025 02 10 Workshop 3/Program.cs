0a57e198258e44f9123810d481a830d3255fe500
Charles <35784256+notxcharles@users.noreply.github.com>
2025-02-10 13:25:16 +0000

    Task3

--- Lecture 3/2025 02 10 Workshop 3/Program.cs
+++ Lecture 3/2025 02 10 Workshop 3/Program.cs
@@ -20,6 +20,32 @@ namespace _2025_02_10_Workshop_3
                 return random.Next(rangeStart, rangeEnd);
             }
         }
+        class Player
+        {
+            private Dice dice;
+            public int score;
+            public Player()
+            {
+                dice = new Dice();
+                score = 0;
+            }
+            public void PlayTurn()
+            {
+                // Computer's roll:
+                int computerRoll = dice.Roll();
+                // Player's roll:
+                int playersRoll = dice.Roll();
+
+                if (playersRoll > computerRoll)
+                {
+                    score += 1;
+                } 
+                else if (playersRoll < computerRoll)
+                {
+                    score -= 1;
+                }
+            }
+        }
         class Game
         {
             public Game()
@@ -34,6 +60,11 @@ namespace _2025_02_10_Workshop_3
         static void Main(string[] args)
         {
             Game game = new Game();
+            Dice dice = new Dice();
+
+            Player player = new Player();
+            player.PlayTurn();
+            Console.WriteLine(player.score);
         }
     }
 }
