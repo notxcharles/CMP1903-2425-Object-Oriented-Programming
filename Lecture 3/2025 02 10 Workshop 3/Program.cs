824e22bc97b96aa1f1009ef103745d08ee820b26
Charles <35784256+notxcharles@users.noreply.github.com>
2025-02-10 13:06:40 +0000

    Task1

---
+++ Lecture 3/2025 02 10 Workshop 3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2025_02_10_Workshop_3
+{
+    internal class Program
+    {
+        class Game
+        {
+            public Game()
+            {
+
+            }
+            public void Start()
+            {
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+        }
+    }
+}
