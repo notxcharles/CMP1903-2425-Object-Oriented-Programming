14b4c877ff60cfe63a8ff2902889e3ddb5632dae
Charles <35784256+notxcharles@users.noreply.github.com>
2025-02-10 13:15:54 +0000

    Task2

--- Lecture 3/2025 02 10 Workshop 3/Program.cs
+++ Lecture 3/2025 02 10 Workshop 3/Program.cs
@@ -10,9 +10,15 @@ namespace _2025_02_10_Workshop_3
     {
         class Game
         {
+            private static Random random;
             public Game()
             {
-
+                random = new Random();
+            }
+            private int Roll(int startRange = 1, int endRange = 6)
+            {
+                int diceRoll = random.Next(startRange, endRange);
+                return diceRoll = 0;
             }
             public void Start()
             {
