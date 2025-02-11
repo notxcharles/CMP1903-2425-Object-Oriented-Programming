9172692e9b9cc192b988db3b76a04b03bf7866f7
Charles <35784256+notxcharles@users.noreply.github.com>
2025-02-11 14:15:18 +0000

    Bank Class
    Bank class will keep track of any accounts that have been opened

--- Bank Account/BankAccount/Program.cs
+++ Bank Account/BankAccount/Program.cs
@@ -207,60 +207,18 @@ namespace BankAccount
         }
     }
 
-    public class BankAccount
+    public class Bank
     {
-        private string m_accountHolder = "";
-        private double m_balance;
-        public BankAccount(string accountHolder)
+        //Class bank should keep track of all accounts that are currently open
+        string m_Name;
+        Bank(string name)
         {
-            this.m_balance = 0;
-            this.m_accountHolder = accountHolder;
-            return;
+            m_Name = name;
         }
-        private double RoundDownValue(double value)
+        public string BankName
         {
-            double newvalue = Math.Floor(value * 100) / 100;
-            return newvalue;
-        }
-        private string DisplayValue(double value)
-        {
-            string valueString = value.ToString("0.00");
-            return valueString;
-        }
-        public double GetBalance()
-        {
-            Console.WriteLine($"Your balance is £{DisplayValue(m_balance)}");
-            return m_balance;
-        }
-
-        public void DepositMoney(double depositValue)
-        {
-            m_balance += depositValue;
-            m_balance = RoundDownValue(m_balance);
-            Console.WriteLine($"Deposited £{DisplayValue(depositValue)}. Your balance is £{DisplayValue(m_balance)}.");
-            return;
-        }
-
-        public void WithdrawMoney(double withdrawAmount)
-        {
-            if (withdrawAmount <= 0)
-            {
-                Console.WriteLine($"Error: You cannot withdraw a negative amount");
-                return;
-            }
-            if (withdrawAmount <= m_balance)
-            {
-                withdrawAmount = RoundDownValue(withdrawAmount);
-                m_balance = m_balance - withdrawAmount;
-                m_balance = RoundDownValue(m_balance);
-                Console.WriteLine($"You have withdrawn £{DisplayValue(withdrawAmount)}. Remaining balance: £{DisplayValue(m_balance)}");
-            }
-            else
-            {
-                withdrawAmount = RoundDownValue(withdrawAmount);
-                Console.WriteLine($"You cannot afford to withdraw £{DisplayValue(withdrawAmount)}. Remaining balance: £{DisplayValue(m_balance)}");
-            }
-            return;
+            get { return m_Name; }
+            set { m_Name = value; }
         }
     }
     internal class Program
