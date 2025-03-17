a65aeb83c3b6e48e7d864a83859ab0fd9e55b8c8
notxcharles <35784256+notxcharles@users.noreply.github.com>
2025-03-17 16:08:21 +0000

    Remove comments

--- Lecture 8/vehicle_system/Bike.cs
+++ Lecture 8/vehicle_system/Bike.cs
@@ -2,21 +2,8 @@
 
 namespace vehicle_system
 {
-    // TODO: Implement inheritance by
-    // making Bike a subclass of Vehicle.
     public class Bike : Vehicle
     {
-        /// TODO: Declare data members specific to the Bike class, 
-        /// following the encapsulation guidelines provided.
-
-
-        // TODO: Implement Bike constructor that calls the
-        // base class constructor.
-
-
-        // TODO: Override the DisplayInfo() method from the
-        // base class to display both Bike-specific attributes
-        // and base class attributes.
         private bool _hasGear;
 
         Bike(string brand, int year, bool hasGear) : base(brand, year)
