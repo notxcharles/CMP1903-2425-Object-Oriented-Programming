506b69990c340bc31d6f4490759c286b0c413c36
notxcharles <35784256+notxcharles@users.noreply.github.com>
2025-03-17 16:25:29 +0000

    Implementation

--- Lecture 8/vehicle_system/Program.cs
+++ Lecture 8/vehicle_system/Program.cs
@@ -9,21 +9,25 @@ namespace vehicle_system
         {
             // TODO: Create vehicle objects, including new Car, new Bike,
             // new Truck, and new Bus
-
-
+            Truck truck1 = new Truck("bigtruck", 4, 2300);
+            Car car1 = new Car("Williams", 2025, 0);
+            Bike bike1 = new Bike("Ducati", 2022, true);
+            Bus bus1 = new Bus("Stagecoach", 2015, 3);
             // TODO: Store the created vehicle objects
             // in a list to test polymorphism
-
+            List<Vehicle> vehicles = new List<Vehicle>();
+            vehicles.Add(truck1);
+            vehicles.Add(car1);
+            vehicles.Add(bike1);
+            vehicles.Add(bus1);
 
             // TODO: Since a Car and a Truck are both Vehicles,
             // loop through the vehicle collection using foreach
             // and display vehicle information via polymorphism. 
             // What are your observations?
-
-
-            foreach (var vehicle in vehicles)
+            foreach (Vehicle vehicle in vehicles)
             {
-                
+                vehicle.DisplayInfo();
             }
 
             
